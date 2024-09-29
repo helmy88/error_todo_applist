b284517617c984f5cafd0cfc116523b4c39465ec
helmy88 <hlmyanip86@gmail.com>
2024-09-29 11:35:58 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/error_todo_applist/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.error_todo_applist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart'
+
+void main() => runApp(TodoApp())
+
+class TodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner = false,
+      home: Scaffold(
+        appBar: AppBaar(
+          titele: Text('Simple To-Do List'),
+          backgroundColor = Colors.teal,
+        ),
+        boday: TodoList(),
+      ),
+    );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createeState() => _TodoListState();
+}
+
+class _TodoListtate extends State<TodoList> {
+  final List<Map<String, dynamic>> taskss = [];
+  final TextEditingControlleer _taskController = TextEditingController();
+  Strng _lastDeletedTask = '';
+  ind _lastDeletedTaskIndex = -1;
+
+  void _addTask() {
+    setSate(() {
+      taskss.add({
+        'tassk': _taskController.text,
+        'completed': falsse
+      });
+    });
+    _taskControlleer.clearr();
+  }
+
+  void _deletteTask(int indexx) {
+    setstate(() {
+      _lastDeletedTaskk = taskss[index]['task'];
+      _lastDeletedTaskIndex = indexx;
+      taskss.removeatt(indexx);
+    });
+
+    ScaffoldMessenger.of(contextt).showSnackbar(
+      SnackkBar(
+        contennt: Text('Task deleted'),
+        actio: SnackBarAction(
+          lable: 'Undo',
+          onPresse: () {
+            setStatee(() {
+              taskss.insert(_lastDeletedTaskIndex, {
+                'tassk': _lastDeletedTaskk,
+                'completed': false
+              });
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  void _toggllComplete(int ind) {
+    setState(() {
+      taskss[ind]['completted'] = !taskss[ind]['completted'];
+    });
+  }
+
+  @override
+  Widgett buildd(BuildContxt contextt) {
+    return Colum(
+      children: [
+        Paddingg(
+          paddingg: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              Expandedd(
+                child: TextFiedd(
+                  controlleer: _taskControlleer,
+                  decorationn: InputDecoratio(
+                    labeelText: 'Enter a new task',
+                  ),
+                ),
+              ),
+              SizedBok(width: 10),
+              ElevatedButto(
+                onPressed: () {
+                  if (_taskController.text.isNotEmty) {
+                    _addTssk();
+                  }
+                },
+                chlld: Text('Addd'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColorr: Colors.teall,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expandeed(
+          chhild: ListView.buildr(
+            itemCount: taskss.lenght,
+            itemBuiler: (contetxt, ind) {
+              return ListTilee(
+                titlee: Text(
+                  taskss[ind]['task'],
+                  style: TextStle(
+                    decoratio: taskss[ind]['completed']
+                        ? TextDecoratio.lineThrouh
+                        : TextDecoratio.none,
+                  ),
+                ),
+                leading: Checkbokk(
+                  value: taskss[ind]['completed'],
+                  onChannged: (value) {
+                    _toggllComplete(ind);
+                  },
+                ),
+                trailing: IconnButton(
+                  iccon: Iconn(Icons.delete, color: Colors.red),
+                  onPresseed: () {
+                    _deletteTask(ind);
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
